cc8b4af68f16910bbde5cd707cb77491e498506c
paradoxalyty <paradoxalyty@gmail.com>
2020-05-22 02:23:04 +0200

    more media queries

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const VCarouselImages = {
     name: 'v-carousel-images',
     props: ['images'],
-    data: () =>({
+    data: () => ({
         activeImage: 0,
     }),
     template: `<div class="v-carousel-image">
@@ -22,20 +22,20 @@ const VCarouselImages = {
                 </div>
             </div>`,
     methods: {
-        prevImage(){
-            if (this.activeImage > 0){
-                this.activeImage -=1;
+        prevImage() {
+            if (this.activeImage > 0) {
+                this.activeImage -= 1;
             }
         },
-        nextImage(){
-            if (this.activeImage < this.images.length - 1){
-                this.activeImage +=1;
+        nextImage() {
+            if (this.activeImage < this.images.length - 1) {
+                this.activeImage += 1;
             }
         },
-        prevBtnClass(){
+        prevBtnClass() {
             return this.activeImage > 0 ? "v-carousel-image__left v-carousel-image__left__active" : "v-carousel-image__left";
         },
-        nextBtnClass(){
+        nextBtnClass() {
             return this.activeImage < this.images.length - 1 ? "v-carousel-image__right v-carousel-image__right__active" : "v-carousel-image__right";
         }
     }
@@ -56,11 +56,11 @@ const vCarouselSizesBlock = {
                 </div>
             </div>`,
     methods: {
-        sizeBlockClass(){
+        sizeBlockClass() {
             return this.size.sizename === this.activeSize ? "v-carousel-sizes-block__sizename v-carousel-sizes-block__sizename__active" : "v-carousel-sizes-block__sizename";
         },
-        setActiveSize(){
-            this.$emit('setActiveSize',  this.size.sizename);
+        setActiveSize() {
+            this.$emit('setActiveSize', this.size.sizename);
         }
     },
 };
@@ -74,14 +74,14 @@ const VCarouselSizes = {
     components: {
         vCarouselSizesBlock
     },
-    data: () =>({
+    data: () => ({
         activeSize: "M",
     }),
     template: `<div class="v-carousel-sizes">
                 <v-carousel-sizes-block v-for="size in sizes" :size="size" :activeSize="activeSize" @setActiveSize = "setActiveSize" :key="size.sizename" />
             </div>`,
-    methods:{
-        setActiveSize(sizeName){
+    methods: {
+        setActiveSize(sizeName) {
             this.activeSize = sizeName;
         }
     }
@@ -107,7 +107,7 @@ const VCarouselItem = {
         VCarouselSizes,
         VCarouselDescriptionListItem
     },
-    data: () =>({
+    data: () => ({
         descriptionOpened: false,
     }),
     template: `<div class="v-carousel-item">
@@ -134,21 +134,21 @@ const VCarouselItem = {
                         <input type="button" class="v-carousel-item__button" value="Заказать"></input>
                     
                 </div>`,
-    methods:{
+    methods: {
         prettify(num) {
             var n = num.toString();
             return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ');
         },
-        changeOpened(){
+        changeOpened() {
             this.descriptionOpened = !this.descriptionOpened;
             // console.log('descriptionOpened', this.descriptionOpened);
         }
     },
-    computed:{
-        descriptionClass(){
+    computed: {
+        descriptionClass() {
             return this.descriptionOpened ? "v-carousel-description" : "v-carousel-description v-carousel-description__hidden";
         },
-        detailsClass(){
+        detailsClass() {
             return this.descriptionOpened ? "v-carousel-item__name__details" : "v-carousel-item__name__details v-carousel-item__name__details__rotated";
         }
     }
@@ -160,11 +160,11 @@ const VCarouselItem = {
  */
 const VCarousel = {
     name: 'v-carousel',
-    props:['artSetsData'], 
+    props: ['artSetsData'],
     components: {
         VCarouselItem,
     },
-    data: () =>({
+    data: () => ({
         activePage: 0,
     }),
     //@wheel="scrollHandle"
@@ -177,18 +177,18 @@ const VCarousel = {
         </div>
     </div>`,
     methods: {
-       
-        navItemClass(itemId){
+
+        navItemClass(itemId) {
             return this.activePage === (itemId - 1) ? "v-carousel-nav-item v-carousel-nav-item__active" : "v-carousel-nav-item";
         },
-       
-        setActivePage(navitemId){
+
+        setActivePage(navitemId) {
             this.activePage = navitemId - 1;
             let inner = document.querySelector('.v-carousel-inner');
             let innerDom = this.$refs.inner;
             inner.scrollLeft = this.activePage * innerDom.scrollWidth / this.pageCount;
         },
-        scrollHandle(event){
+        scrollHandle(event) {
             event.preventDefault();
             event.currentTarget.scrollLeft += event.deltaY;
             let pageWidth = (event.currentTarget.scrollWidth - event.currentTarget.clientWidth) / this.pageCount;
@@ -197,7 +197,7 @@ const VCarousel = {
             if (currPage < 0) {
                 this.activePage = 0
             } else if (currPage >= this.pageCount) {
-                this.activePage = this.pageCount -1;
+                this.activePage = this.pageCount - 1;
 
             } else {
                 this.activePage = currPage;
@@ -208,24 +208,24 @@ const VCarousel = {
 
     },
     computed: {
-        isDesctop(){
+        isDesctop() {
             return screen.width > 767 ? true : false
         },
-        pageItems(){
+        pageItems() {
             let itemsCount = this.isDesctop ? 3 : 1;
             return this.artSetsData.slice(this.activePage * itemsCount, this.activePage * itemsCount + itemsCount)
         },
-        pageCount(){
+        pageCount() {
             return this.isDesctop ? Math.ceil(this.artSetsData.length / 3) : this.artSetsData.length;
-            
+
         },
-        
+
     }
 };
 
 const app = new Vue({
     el: '#app',
-    data:{
+    data: {
         artSetsData: [
             {
                 id: 1,
@@ -282,14 +282,12 @@ const app = new Vue({
                 currentSize: 30
             }
         ]
-        
+
     },
-    components:{
+    components: {
         VCarousel,
     },
-    methods:{
-        
-    },
+    methods: {},
     // computed:{
     //     isDesctop(){
     //         return screen.width > 767 ? true : false;
@@ -298,12 +296,10 @@ const app = new Vue({
 });
 
 
-
-
 window.onload = window.onresize = setStyle;
 
 function setStyle() {
-    let elements = [...document.querySelectorAll('.howItWorks-text')];
+    let elements = [...document.querySelectorAll('.text')];
 
     if (window.matchMedia("(min-width: 576px)").matches) {
         elements.forEach(item => {
@@ -316,18 +312,26 @@ function setStyle() {
     }
 }
 
-document.querySelectorAll(".howItWorks-btn").forEach(function (element) {
+document.querySelectorAll(".check-btn").forEach(function (element) {
     element.addEventListener("click", function () {
 
             let display = this.parentNode.querySelector("p").style.display;
 
             if (display === "block") {
                 this.parentNode.querySelector("p").style.display = "none";
-                element.removeAttribute('open');
             } else {
                 this.parentNode.querySelector("p").style.display = "block";
-                element.setAttribute('open', 'open');
             }
+
+            let lastItem = this.parentNode.classList.contains("lastItem");
+
+            if (lastItem && !element.parentElement.hasAttribute('open')) {
+                element.parentElement.setAttribute('open', 'open');
+            } else if (lastItem && element.parentElement.hasAttribute('open')) {
+                element.parentElement.removeAttribute('open');
+            }
+
+
         },
         false)
 });
